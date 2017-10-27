d6cc94ec49c282677fcad9c34f17f605f3f2fe97
AMartinez2 <ausmartinez@csumb.edu>
2017-10-27 00:07:29 -0700

    file transfer

--- js/app.js
+++ js/app.js
@@ -16,33 +16,61 @@
 
 // Basically our model ////////////////////
 
-// To hold all data regarding each marker passed from Maps API
-function Marker(value){
-  var self = this;
-  // May need more data variables
-  self.data = value;
-}
 
 var map;
+var locationResult;
+
 function initMap() {
   var pyrmont = {lat: -33.867, lng: 151.195};
   // Constructor creates a new map - only center and zoom are required
   map = new google.maps.Map(document.getElementById('map'), {
     // {lat: 40.7413549, lng: -73.9980244},
     center: pyrmont,
-    zoom: 13
+    zoom: 14
   });
 
   // This autocomplete is for use in the geocoder entry box.
-  var inputAreaComplete = new google.maps.places.Autocomplete(
-      document.getElementById('input-location'));
+  // var inputAreaComplete = new google.maps.places.Autocomplete(
+  //     document.getElementById('input-location'));
 
+  // Geo code given information
+  var geocoder = new google.maps.Geocoder();
+  document.getElementById('submit-location').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
 
+  // Get relevant places near the searched area
   var places = new google.maps.places.PlacesService(map);
   places.nearbySearch({
     location: pyrmont,
     radius: 500
   }, callback);
+}
+
+//TODO work on integrating the geocode function into the acutal flow of the app
+
+// Geocode a given address
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('input-location').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status == 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      // var marker = new google.maps.Marker({
+      //   map: resultsMap,
+      //   position: results[0].geometry.location
+      // });
+    }
+    else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+// To hold all data regarding each marker passed from Maps API
+function Marker(value){
+  var self = this;
+  // May need more data variables
+  self.data = value;
 }
 
 function callback(results, status) {
@@ -61,8 +89,10 @@ function createMarker(place) {
   });
 }
 
+function update(inputLocation) {
 
-// Our viewmodel ////////////////////////
+}
+
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
     var self = this;
@@ -75,9 +105,9 @@ function AppViewModel() {
     // TODO When we start getting marker info from maps, we will need to flush
     //        the markers array and then refill it with necessary info
     self.addToMarkers = function() {
-      self.markers.push( new Marker(self.inputLocation()) )
+      update(self.inputLocation());
+      // self.markers.push( new Marker(self.inputLocation()) );
     }
 }
-
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
